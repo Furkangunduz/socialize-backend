40da78aafb4082a3eb0dbf27fb1de19c15288da8
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-03-27 20:54:05 +0300

    refactor: data validation to include query, params, body

--- src/middlewares/validateRequest.middleware.js
+++ src/middlewares/validateRequest.middleware.js
@@ -1,7 +1,9 @@
 const { ApiError } = require('../helpers/ApiError');
 
 const validateRequest = (schema) => async (req, res, next) => {
-  const { error } = schema.validate(req.body);
+  const dataToValidate = Object.assign({}, req.body, req.query, req.params);
+
+  const { error } = schema.validate(dataToValidate);
   if (error) {
     return res.status(400).json(new ApiError(400, 'Validation Error', error.details.map((e) => e.message).join(', ')));
   }
