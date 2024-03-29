5699853b84e6f25fadc9c8508dfcd9af556b3242
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-03-29 19:52:22 +0300

    feat: add multer middleware to parse formdata

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const authRoute = require('./routes/auth.route.js');
 const userRoute = require('./routes/user.route.js');
 
 const app = express();
+const upload = multer();
+
 app.use(morgan('dev'));
 app.use(
   cors({
@@ -19,6 +21,7 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use('/assets', express.static('assets'));
+app.use(upload.none());
 
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
